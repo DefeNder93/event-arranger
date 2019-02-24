cb1948cdfe146323493763eb820a68d1c5337119
DefeNder93 <alexdefender93@gmail.com>
2019-02-24 14:14:52 +0300

    Frontend: search feature was implemented

--- frontend/src/app/user/components/user/user.component.ts
+++ frontend/src/app/user/components/user/user.component.ts
@@ -5,8 +5,9 @@ import {State} from "../../user.state";
 import {select, Store} from "@ngrx/store";
 import {selectAllUsers, selectEditingIds} from "./user.selectors";
 import {ActionUserDelete, ActionUserToggleEditState, ActionUserUpdate} from "./user.actions";
-import {map, withLatestFrom} from "rxjs/operators";
-import {FormBuilder} from "@angular/forms";
+import {map, tap, distinctUntilChanged, debounceTime, startWith} from "rxjs/operators";
+import {FormBuilder, FormControl} from "@angular/forms";
+import {combineLatest} from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -17,6 +18,12 @@ import {FormBuilder} from "@angular/forms";
 export class UserComponent implements OnInit {
 
   userRows$ : Observable<UserRow[]>;
+  search = new FormControl('');
+  search$ = this.search.valueChanges.pipe(
+    debounceTime(500),
+    distinctUntilChanged(),
+    startWith('')
+  );
 
   constructor(private store: Store<State>, private fb: FormBuilder) {
     this.loadUserRows();
@@ -44,19 +51,18 @@ export class UserComponent implements OnInit {
 
   private toggleEditState = (user: UserRow) => this.store.dispatch(new ActionUserToggleEditState({ id: user.id }));
 
-  private buildUserRows = (ids, users) => {
+  private buildUserRows = (ids, users, search) => {
     const userRows: UserRow[] = users;
     userRows.forEach(e => e.editing = ids.indexOf(e.id) !== -1);
-    return userRows;
+    return search ? userRows.filter(e => e.firstName.toLowerCase().indexOf(search.toLowerCase()) !== -1) : userRows;
   }
 
   private loadUserRows = () => {
     const users$: Observable<User[]> = this.store.pipe(select(selectAllUsers));
     const editingIds$: Observable<string[]> = this.store.pipe(select(selectEditingIds));
 
-    this.userRows$ = editingIds$.pipe(
-      withLatestFrom(users$),
-      map(([ids, users]) => this.buildUserRows(ids, users))
+    this.userRows$ = combineLatest(editingIds$, users$, this.search$).pipe(
+      map(([ids, users, search]) => this.buildUserRows(ids, users, search))
     );
   }
 
